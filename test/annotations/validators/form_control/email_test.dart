0ea221c421c20cb2fcd9d120494a9fdc8db0b975
maxime-aubry <maxime.aubry.76@gmail.com>
2021-02-19 16:09:21 +0100

    add tests

--- test/annotations/validators/form_control/email_test.dart
+++ test/annotations/validators/form_control/email_test.dart
@@ -5,19 +5,19 @@ import 'stubs/email.dart';
 void main() {
   group('Annotations > Validators > FormControl<TProperty> > Email.', () {
     group('Valid.', () {
-      test('Value is a valid email.', () async {
+      test('value is a valid email.', () async {
         Email_ValidEmail_Stub stub = new Email_ValidEmail_Stub();
         bool isValid = await stub.validator.isValid(stub.control);
         expect(isValid, isTrue);
       });
 
-      test('Value is null.', () async {
+      test('value is null.', () async {
         Email_ValueIsNull_Stub stub = new Email_ValueIsNull_Stub();
         bool isValid = await stub.validator.isValid(stub.control);
         expect(isValid, isTrue);
       });
 
-      test('Value is empty.', () async {
+      test('value is empty.', () async {
         Email_EmptyValue_Stub stub = new Email_EmptyValue_Stub();
         bool isValid = await stub.validator.isValid(stub.control);
         expect(isValid, isTrue);
@@ -25,7 +25,7 @@ void main() {
     });
 
     group('Invalid.', () {
-      test('Value is not a valid email.', () async {
+      test('value is not a valid email.', () async {
         Email_InavlidEmail_Stub stub = new Email_InavlidEmail_Stub();
         bool isValid = await stub.validator.isValid(stub.control);
         expect(isValid, isFalse);
--- test/annotations/validators/form_control/equal_to_datetime_test.dart
+++ test/annotations/validators/form_control/equal_to_datetime_test.dart
@@ -1,22 +1,56 @@
 import 'package:flutter_test/flutter_test.dart';
 
+import 'stubs/equal_to_datetime.dart';
+
 void main() {
   group('Annotations > Validators > FormControl<TProperty> > EqualToDateTime.',
       () {
     group('Valid.', () {
-      test('', () async {});
+      test('value equals valueToCompare.', () async {
+        EqualToDateTime_ValueEqualsValueToCompare_Stub stub =
+            new EqualToDateTime_ValueEqualsValueToCompare_Stub();
+        bool isValid = await stub.validator.isValid(stub.control);
+        expect(isValid, isTrue);
+      });
+
+      test('value and valueToCompare are null.', () async {
+        EqualToDateTime_ValueAndValueToCompareAreNull_Stub stub =
+            new EqualToDateTime_ValueAndValueToCompareAreNull_Stub();
+        bool isValid = await stub.validator.isValid(stub.control);
+        expect(isValid, isTrue);
+      });
     });
 
     group('Invalid.', () {
-      test('', () async {});
+      test('value is null.', () async {
+        EqualToDateTime_ValueIsNull_Stub stub =
+            new EqualToDateTime_ValueIsNull_Stub();
+        bool isValid = await stub.validator.isValid(stub.control);
+        expect(isValid, isFalse);
+      });
+
+      test('valueToCompare is null.', () async {
+        EqualToDateTime_ValueToCompareIsNull_Stub stub =
+            new EqualToDateTime_ValueToCompareIsNull_Stub();
+        bool isValid = await stub.validator.isValid(stub.control);
+        expect(isValid, isFalse);
+      });
+
+      test('value does not equal valueToCompare.', () async {
+        EqualToDateTime_ValueDoesNotEqualValueToCompare_Stub stub =
+            new EqualToDateTime_ValueDoesNotEqualValueToCompare_Stub();
+        bool isValid = await stub.validator.isValid(stub.control);
+        expect(isValid, isFalse);
+      });
     });
 
     group('Remote parameters.', () {
-      test('', () async {});
-    });
-
-    group('Throws exception on parameters.', () {
-      test('', () async {});
+      test('Remot valueToCompare is provided.', () async {
+        EqualToDateTime_remoteValueToCompareIsProvided_Stub stub =
+            new EqualToDateTime_remoteValueToCompareIsProvided_Stub();
+        bool isValid = await stub.validator.isValid(stub.control);
+        expect(isValid, isTrue);
+      });
     });
   });
 }
